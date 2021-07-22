3f5505fb26de1ea9f5ad519757fe06b73928104b
Prakash Vishwakarma <prakash2033@outlook.com>
2021-07-22 16:33:49 +0200

    Get row and Get All

--- dao.js
+++ dao.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3');
 const Promise = require('bluebird');
+const { resolve, reject } = require('bluebird');
 
 class AppDAO {
     constructor(dbFilePath) {
@@ -20,9 +21,37 @@ class AppDAO {
                     console.log(err);
                     reject(err);
                 } else {
-                    resolve({id: this.lastID});
+                    resolve({ id: this.lastID });
                 }
             });
+        });
+    }
+
+    get(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, params, (err, result) => {
+                if (err) {
+                    console.log('Error running sql:' + sql);
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    all(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, rows) => {
+                if (err) {
+                    console.log('Error running sql: ' + sql);
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            })
         });
     }
 }
